24a94108259a01cb7bbf485203a701c3c04602e0
May_Edu <mayck22.03@hotmail.com>
2023-05-27 13:14:09 -0300

    tema claro e escuro
    testando o tema claro e escuro no index.html

---
+++ backlog/js/script.js
@@ -0,0 +1,5 @@
+function on_off() {
+    if(document.getElementById("ligth-dark").attributes = "checked"){
+        document.body.classList.toggle("white")
+    }
+}
---
+++ backlog/testes/JS/script.js
@@ -0,0 +1,6 @@
+function on_off() {
+    
+    if(document.getElementById("ligth-dark").attributes = "checked"){
+        document.body.classList.toggle("white")
+    }
+}
